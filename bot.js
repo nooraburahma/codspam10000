247fadb50a2a4496a2859098d97ee796b0225e0c
nooraburahma <47223988+nooraburahma@users.noreply.github.com>
2019-03-21 19:27:36 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("550650623826329603")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`اللهم صلي على سيدنا محمد اللهم صلي على سيدنا محمد`);
 }, 30)
 })
 
